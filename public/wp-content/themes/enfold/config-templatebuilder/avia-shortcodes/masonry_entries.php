b1061b40ca1f0d1b674d0f65926de52897db6ab2
irthos <irth03@gmail.com>
2014-11-21 18:12:15 -0800

    commit

--- public/wp-content/themes/enfold/config-templatebuilder/avia-shortcodes/masonry_entries.php
+++ public/wp-content/themes/enfold/config-templatebuilder/avia-shortcodes/masonry_entries.php
@@ -589,12 +589,16 @@ if ( !class_exists( 'avia_masonry' ) )
                         $markup = avia_markup_helper(array('context' => 'entry_title','echo'=>false, 'id'=>$entry['ID'], 'custom_markup'=>$this->atts['custom_markup']));
 						$items .=	"<h3 class='av-masonry-entry-title entry-title' {$markup}>{$the_title}</h3>";
 					}
-
-					if($post->post_region){ $html .= '<span itemprop="addressRegion">'.$post->post_region.'</span><br>';}
-
 					if(strpos($this->atts['caption_elements'], 'excerpt') !== false && !empty($content)){
+                                            $markup = avia_markup_helper(array('context' => 'entry_content','echo'=>false, 'id'=>$entry['ID'], 'custom_markup'=>$this->atts['custom_markup']));
+                    						$items .=	"<div class='av-masonry-entry-content entry-content' {$markup}>{$content}</div>";
+                    					}
+
+					if(){ $html .= '<span itemprop="addressRegion">'.$post->post_region.'</span><br>';}
+
+					if($post->post_region){
                         $markup = avia_markup_helper(array('context' => 'entry_content','echo'=>false, 'id'=>$entry['ID'], 'custom_markup'=>$this->atts['custom_markup']));
-						$items .=	"<div class='av-masonry-entry-content entry-content' {$markup}>{$content}</div>";
+						$items .=	"<span itemprop="addressRegion">'.$post->post_region.'</span><br><div class='av-masonry-entry-content entry-content' {$markup}>{$content}</div>";
 					}
 
 
