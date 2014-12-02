e7afb07f4f2795c4a0cc1c5d2e2ca0768445c2ae
irthos <irth03@gmail.com>
2014-12-01 16:08:24 -0800

    masonry editing

--- public/wp-content/themes/enfold/config-templatebuilder/avia-shortcodes/masonry_entries.php
+++ public/wp-content/themes/enfold/config-templatebuilder/avia-shortcodes/masonry_entries.php
@@ -597,7 +597,7 @@ if ( !class_exists( 'avia_masonry' ) )
                         $markup = avia_markup_helper(array('context' => 'entry_content','echo'=>false, 'id'=>$entry['ID'], 'custom_markup'=>$this->atts['custom_markup']));
 						$items .=	"<div class='av-masonry-entry-content entry-content' {$markup}>{$content}</div>";
 					}
-					if(strpos($this->atts['caption_elements'], 'location') !== false && !empty($content)){
+					if(strpos($this->atts['caption_elements'], 'location') !== false){
                         $markup = avia_markup_helper(array('context' => 'entry_location','echo'=>false, 'id'=>$entry['ID'], 'custom_markup'=>$this->atts['custom_markup']));
                         $post_details = geodir_get_post_info($post_id);
                         print_r($post_details);
