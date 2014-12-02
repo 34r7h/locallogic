2169543c95b88e5ea8df25f4ded424ac60158cd4
irthos <irth03@gmail.com>
2014-12-02 14:54:27 -0800

    masonry editing

--- public/wp-content/themes/enfold/config-templatebuilder/avia-shortcodes/masonry_entries.php
+++ public/wp-content/themes/enfold/config-templatebuilder/avia-shortcodes/masonry_entries.php
@@ -592,14 +592,12 @@ if ( !class_exists( 'avia_masonry' ) )
 
 					if(strpos($this->atts['caption_elements'], 'excerpt') !== false && !empty($content)){
                         $markup = avia_markup_helper(array('context' => 'entry_content','echo'=>false, 'id'=>$entry['ID'], 'custom_markup'=>$this->atts['custom_markup']));
-                        $post_details = geodir_get_post_info($post_id);
-                        print_r($post_details);
-						$items .=	"<div class='av-masonry-entry-content entry-content' {$markup}>{$post_details}<?php geodir_get_post_info($post_id); print_r($post_details); ?>{$content}</div>";
+						$items .=	"<div class='av-masonry-entry-content entry-content' {$markup}>{$content}</div>";
 						$items .=   geodir_get_post_info($entry['ID']);
 
 					}
 
-					$items .=	$text_after."</div></figcaption>" + geodir_get_post_info($entry['ID']);
+					$items .=	$text_after."</div></figcaption>";
 				}
 				$items .= 		"</figure>";
 				$items .= 	"</{$html_tags[1]}><!--end av-masonry entry-->";					
