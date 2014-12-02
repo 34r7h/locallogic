d5bd085ae0f296acb8cdce286f0f3cc5de14740b
irthos <irth03@gmail.com>
2014-12-02 15:05:20 -0800

    masonry editing

--- public/wp-content/themes/enfold/config-templatebuilder/avia-shortcodes/masonry_entries.php
+++ public/wp-content/themes/enfold/config-templatebuilder/avia-shortcodes/masonry_entries.php
@@ -592,8 +592,7 @@ if ( !class_exists( 'avia_masonry' ) )
 
 					if(strpos($this->atts['caption_elements'], 'excerpt') !== false && !empty($content)){
                         $markup = avia_markup_helper(array('context' => 'entry_content','echo'=>false, 'id'=>$entry['ID'], 'custom_markup'=>$this->atts['custom_markup']));
-						$items .=	"<div class='av-masonry-entry-content entry-content' {$markup}>{$content}</div>";
-						$items .=   geodir_get_post_info($entry['ID']);
+						$items .=	"<div class='av-masonry-entry-content entry-content' {$markup}>{$content}</div>" + geodir_get_post_info($entry['ID']);
 
 					}
 
