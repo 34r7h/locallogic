ec6c0c1887e381f1be30c5e218d57e2d8692b4da
irthos <irth03@gmail.com>
2014-12-02 15:00:08 -0800

    masonry editing

--- public/wp-config.php
+++ public/wp-config.php
@@ -121,7 +121,7 @@ define('WPLANG', '');
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 
 define( 'WP_ALLOW_MULTISITE', true );
 define('MULTISITE', true);
